aaa98a2207c3df918fd5db8cd6845449e45cc481
Ivan Dadalto <devivandalto@Ivans-MacBook-Pro.local>
2024-07-21 17:29:52 -0300

    update configs

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,11 +1,19 @@
-require('dotenv').config();
 
 module.exports = {
   development: {
     username: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'postgres',
+    port: process.env.DB_PORT || 5432,
+    dialect: 'postgres'
   },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: 'postgres'
+  }
 };
