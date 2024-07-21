f74c72531badb7bba89a72d2306fc1b469c11ff4
Ivan Dadalto <devivandalto@Ivans-MacBook-Pro.local>
2024-07-21 20:58:53 -0300

    update ssl

--- backend/config/database.js
+++ backend/config/database.js
@@ -6,7 +6,6 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false,
     }
   }
 });
