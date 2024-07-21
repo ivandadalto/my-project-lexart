c5ea5d11700911e0061e29d672d37ff13956f297
Ivan Dadalto <devivandalto@Ivans-MacBook-Pro.local>
2024-07-21 20:54:25 -0300

    setting up env configs

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
     port: process.env.DB_PORT || 5432,
     dialect: 'postgres'
   }
