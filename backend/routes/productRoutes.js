cefdc0f8f7ef767dfbd61b8df99ee6727859c67e
Ivan Dadalto <devivandalto@Ivans-MacBook-Pro.local>
2024-07-21 13:27:49 -0300

    configs de deploy

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,7 +4,7 @@ const { authenticate } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 router.post('/', authenticate, createProduct);
-router.get('/', authenticate, getProducts);
+router.get('/', getProducts);
 router.get('/:id', authenticate, getProduct);
 router.put('/:id', authenticate, updateProduct);
 router.delete('/:id', authenticate, deleteProduct);
