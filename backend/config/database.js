c0ef130b60dda9853b06fcca52da3cc738319da5
Ivan Dadalto <devivandalto@Ivans-MacBook-Pro.local>
2024-07-21 20:57:39 -0300

    set up ssl

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,6 +3,12 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    }
+  }
 });
 
 sequelize.authenticate()
