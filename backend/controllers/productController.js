6263d70d35a56bebcc6983817d9a620d9cf9e132
Ivan Dadalto <devivandalto@Ivans-MacBook-Pro.local>
2024-07-21 22:35:02 -0300

    test changes productcontroller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,43 +1,43 @@
 const { Product } = require('../models');
 
-// const createProduct = async (req, res) => {
-//   try {
-//     const { name, brand, model, price, color } = req.body;
-//     const product = await Product.create({ name, brand, model, price, color });
-//     res.status(201).json(product);
-//   } catch (error) {
-//     res.status(400).json({ error: error.message });
-//   }
-// };
-
 const createProduct = async (req, res) => {
   try {
-    const data = req.body;
-
-    if (Array.isArray(data)) {
-      const products = await Promise.all(data.map(async (item) => {
-        const { name, brand, model, data: productData } = item;
-        return Promise.all(productData.map(async (entry) => {
-          const product = await Product.create({ name, brand, model, price: entry.price, color: entry.color });
-          return product;
-        }));
-      }));
-
-      res.status(201).json({ message: 'Produtos inseridos com sucesso', products: products.flat() });
-    } else if (data.details) {
-      const { name, details, price } = data;
-      const { brand, model, color } = details;
-      const product = await Product.create({ name, brand, model, price, color });
-      res.status(201).json({ message: 'Produto inserido com sucesso', product });
-    } else {
-      const { name, brand, model, price, color } = data;
-      const product = await Product.create({ name, brand, model, price, color });
-      res.status(201).json({ message: 'Produto inserido com sucesso', product });
-    }
+    const { name, brand, model, price, color } = req.body;
+    const product = await Product.create({ name, brand, model, price, color });
+    res.status(201).json(product);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 };
+
+// const createProduct = async (req, res) => {
+//   try {
+//     const data = req.body;
+
+//     if (Array.isArray(data)) {
+//       const products = await Promise.all(data.map(async (item) => {
+//         const { name, brand, model, data: productData } = item;
+//         return Promise.all(productData.map(async (entry) => {
+//           const product = await Product.create({ name, brand, model, price: entry.price, color: entry.color });
+//           return product;
+//         }));
+//       }));
+
+//       res.status(201).json({ message: 'Produtos inseridos com sucesso', products: products.flat() });
+//     } else if (data.details) {
+//       const { name, details, price } = data;
+//       const { brand, model, color } = details;
+//       const product = await Product.create({ name, brand, model, price, color });
+//       res.status(201).json({ message: 'Produto inserido com sucesso', product });
+//     } else {
+//       const { name, brand, model, price, color } = data;
+//       const product = await Product.create({ name, brand, model, price, color });
+//       res.status(201).json({ message: 'Produto inserido com sucesso', product });
+//     }
+//   } catch (error) {
+//     res.status(400).json({ error: error.message });
+//   }
+// };
 
 const getProducts = async (req, res) => {
   try {
