f5a174731f681be341ec14067e28372a3549418e
Ivan Dadalto <devivandalto@Ivans-MacBook-Pro.local>
2024-07-21 23:37:50 -0300

    test change base url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://my-project-lexart-backend-5cv73detn-ivans-projects-398b638a.vercel.app/api',
+  baseURL: 'https://my-project-lexart-backend-5cv73detn-ivans-projects-398b638a.vercel.app/',
 });
 
 api.interceptors.request.use(config => {
