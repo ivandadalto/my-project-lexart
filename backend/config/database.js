d9f1c263449d243228a16ea2a5c9cc7dcd4c14fe
Ivan Dadalto <devivandalto@Ivans-MacBook-Pro.local>
2024-07-21 21:51:09 -0300

    change database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,8 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize, DATE } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const DATABASE_URL  = process.env.DATABASE_URL
+
+const sequelize = new Sequelize(DATABASE_URL, {
   host: process.env.DATABASE_URL,
   dialect: 'postgres',
   dialectOptions: {
