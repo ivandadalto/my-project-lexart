645cf87b8601155b29f1ce188db88c3e188a7ab7
Ivan Dadalto <devivandalto@Ivans-MacBook-Pro.local>
2024-07-21 21:30:48 -0300

    fix 3

--- backend/config/config.js
+++ backend/config/config.js
@@ -6,7 +6,15 @@ module.exports = {
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DATABASE,
     host: process.env.POSTGRES_HOST,
-    port: process.env.DB_PORT || 5432,
+    port: 5432,
+    dialect: 'postgres'
+  },
+  production: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
+    port: 5432,
     dialect: 'postgres'
   }
 }
--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  host: process.env.DB_HOST,
+  host: process.env.DATABASE_URL,
   dialect: 'postgres',
   dialectOptions: {
     ssl: {
