42f96e7f15479403633aea5a26cfab3259fe32a5
Ivan Dadalto <devivandalto@Ivans-MacBook-Pro.local>
2024-07-21 21:17:36 -0300

    fix

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.POSTGRES_DATABASE, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   host: process.env.DB_HOST,
   dialect: 'postgres',
   dialectOptions: {
