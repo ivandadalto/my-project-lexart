3acfdafcf959b977a28b6252c4f34a2faf34d105
Ivan Dadalto <devivandalto@Ivans-MacBook-Pro.local>
2024-07-21 21:34:49 -0300

    fix 4

--- backend/config/config.js
+++ backend/config/config.js
@@ -6,15 +6,23 @@ module.exports = {
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DATABASE,
     host: process.env.POSTGRES_HOST,
-    port: 5432,
-    dialect: 'postgres'
+    port: process.env.POSTGRES_PORT || 5432,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+    }
   },
   production: {
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DATABASE,
-    host: process.env.POSTGRES_HOST,
-    port: 5432,
-    dialect: 'postgres'
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
+}
 }
