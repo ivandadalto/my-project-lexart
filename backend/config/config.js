ccfa91de7ffc0464919291e11829521ff3f8edf0
Ivan Dadalto <devivandalto@Ivans-MacBook-Pro.local>
2024-07-22 11:25:23 -0300

    try fix

--- backend/config/config.js
+++ backend/config/config.js
@@ -16,6 +16,11 @@ module.exports = {
   },
   production: {
     url: process.env.DATABASE_URL,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT || 5432,
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
